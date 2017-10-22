3218d1560ff5259ecc69f23afee4cdff4d3fbb35
Derek Illchuk <derek@illchuk.com>
2017-10-21 19:24:47 -0500

    Useful tip

--- src/Sender/Blackhole.php
+++ src/Sender/Blackhole.php
@@ -7,7 +7,10 @@ use ZfcUserForgotPassword\Entity\Reset;
 class Blackhole implements SenderInterface {
 
     public function send($email, Reset $reset) {
-
+        /**
+         * In your implementation, you'll want to direct the user to:
+         * <scheme/host>/user/reset_password/<userID>/<nonce>
+         */
     }
 
 }
