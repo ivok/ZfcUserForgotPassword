8cadc1969c749b42e944552627036a162524966b
Derek Illchuk <derek@illchuk.com>
2017-10-24 15:54:52 -0500

    Culled the doctrine config.

--- test/doctrine.php
+++ test/doctrine.php
@@ -3,75 +3,16 @@
 return [
     'doctrine' => [
         'cache' => [
-            'apc' => [
-                'class' => \Doctrine\Common\Cache\ApcCache::class,
-                'namespace' => 'DoctrineModule',
-            ],
-            'apcu' => [
-                'class' => Doctrine\Common\Cache\ApcuCache::class,
-                'namespace' => 'DoctrineModule',
-            ],
             'array' => [
                 'class' => Doctrine\Common\Cache\ArrayCache::class,
                 'namespace' => 'DoctrineModule',
             ],
-            'filesystem' => [
-                'class' => Doctrine\Common\Cache\FilesystemCache::class,
-                'directory' => 'data/DoctrineModule/cache',
-                'namespace' => 'DoctrineModule',
-            ],
-            'memcache' => [
-                'class' => Doctrine\Common\Cache\MemcacheCache::class,
-                'instance' => 'my_memcache_alias',
-                'namespace' => 'DoctrineModule',
-            ],
-            'memcached' => [
-                'class' => Doctrine\Common\Cache\MemcachedCache::class,
-                'instance' => 'my_memcached_alias',
-                'namespace' => 'DoctrineModule',
-            ],
-            'predis' => [
-                'class' => Doctrine\Common\Cache\PredisCache::class,
-                'instance' => 'my_predis_alias',
-                'namespace' => 'DoctrineModule',
-            ],
-            'redis' => [
-                'class' => Doctrine\Common\Cache\RedisCache::class,
-                'instance' => 'my_redis_alias',
-                'namespace' => 'DoctrineModule',
-            ],
-            'wincache' => [
-                'class' => Doctrine\Common\Cache\WinCacheCache::class,
-                'namespace' => 'DoctrineModule',
-            ],
-            'xcache' => [
-                'class' => Doctrine\Common\Cache\XcacheCache::class,
-                'namespace' => 'DoctrineModule',
-            ],
-            'zenddata' => [
-                'class' => Doctrine\Common\Cache\ZendDataCache::class,
-                'namespace' => 'DoctrineModule',
-            ],
-        ],
-        'authenticationadapter' => [
-            'odm_default' => true,
-            'orm_default' => true,
-        ],
-        'authenticationstorage' => [
-            'odm_default' => true,
-            'orm_default' => true,
-        ],
-        'authenticationservice' => [
-            'odm_default' => true,
-            'orm_default' => true,
         ],
         'connection' => [
             'orm_default' => [
                 'configuration' => 'orm_default',
                 'eventmanager' => 'orm_default',
                 'params' => [
-                    'host' => 'localhost',
-                    'port' => '3306',
                     'user' => 'travis',
                     'password' => '',
                     'url' => 'pdo-mysql://travis@localhost/zfcuserforgotpassword?charset=utf8',
@@ -80,22 +21,7 @@ return [
             ],
         ],
         'configuration' => [
-            'orm_default' => [
-                'metadata_cache' => 'array',
-                'query_cache' => 'array',
-                'result_cache' => 'array',
-                'hydration_cache' => 'array',
-                'driver' => 'orm_default',
-                'generate_proxies' => true,
-                'proxy_dir' => 'data/DoctrineORMModule/Proxy',
-                'proxy_namespace' => 'DoctrineORMModule\\Proxy',
-                'filters' => [],
-                'datetime_functions' => [],
-                'string_functions' => [],
-                'numeric_functions' => [],
-                'second_level_cache' => [],
-                'filter_schema_assets_expression' => '',
-            ],
+            'orm_default' => [],
         ],
         'driver' => [
             'orm_default' => [
@@ -107,12 +33,10 @@ return [
             ],
             'ZfcUserForgotPassword_driver' => [
                 'class' => \Doctrine\ORM\Mapping\Driver\AnnotationDriver::class,
-                'cache' => 'array',
                 'paths' => __DIR__ . '/../src/Entity',
             ],
             'ZfcUser_driver' => [
                 'class' => \Doctrine\ORM\Mapping\Driver\XmlDriver::class,
-                'cache' => 'array',
                 'paths' => __DIR__ . '/../vendor/zf-commons/zfc-user-doctrine-orm/config/xml/zfcuser',
             ],
         ],
@@ -123,17 +47,6 @@ return [
             ],
         ],
         'eventmanager' => [
-            'orm_default' => [
-                'subscribers' => [],
-            ],
-        ],
-        'sql_logger_collector' => [
-            'orm_default' => [],
-        ],
-        'mapping_collector' => [
-            'orm_default' => [],
-        ],
-        'formannotationbuilder' => [
             'orm_default' => [],
         ],
         'entity_resolver' => [
@@ -141,9 +54,6 @@ return [
         ],
     ],
     'service_manager' => [
-        'factories' => [
-            'Doctrine\ORM\EntityManager' => \DoctrineORMModule\Service\EntityManagerAliasCompatFactory::class,
-        ],
         'abstract_factories' => [
             'DoctrineModule' => \DoctrineModule\ServiceFactory\AbstractDoctrineServiceFactory::class,
         ],
@@ -157,16 +67,8 @@ return [
         'configuration' => \DoctrineORMModule\Service\ConfigurationFactory::class,
         'entitymanager' => \DoctrineORMModule\Service\EntityManagerFactory::class,
         'entity_resolver' => \DoctrineORMModule\Service\EntityResolverFactory::class,
-        'sql_logger_collector' => \DoctrineORMModule\Service\SQLLoggerCollectorFactory::class,
-        'mapping_collector' => \DoctrineORMModule\Service\MappingCollectorFactory::class,
-        'formannotationbuilder' => \DoctrineORMModule\Service\FormAnnotationBuilderFactory::class,
-        'migrations_configuration' => \DoctrineORMModule\Service\MigrationsConfigurationFactory::class,
-        'migrations_cmd' => \DoctrineORMModule\Service\MigrationsCommandFactory::class,
         'cache' => \DoctrineModule\Service\CacheFactory::class,
         'eventmanager' => \DoctrineModule\Service\EventManagerFactory::class,
         'driver' => \DoctrineModule\Service\DriverFactory::class,
-        'authenticationadapter' => \DoctrineModule\Service\Authentication\AdapterFactory::class,
-        'authenticationstorage' => \DoctrineModule\Service\Authentication\StorageFactory::class,
-        'authenticationservice' => \DoctrineModule\Service\Authentication\AuthenticationServiceFactory::class,
     ],
 ];
